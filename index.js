de2d9687930de6167b38f9c2626dc9c5dac4ae69
kee-siang <tksiang.ss@example.com>
2023-06-15 16:39:56 +0800

    filter out the return password section and then set error message for email

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 
 import express from "express"
 import prisma from "./src/utils/prisma.js"
+import cors from 'cors';
+import { Prisma } from "@prisma/client";
 
 const app = express()
 const port = process.env.PORT || 8080
 app.use(express.json())
+app.use(cors());
 
-//get data api//
+// get data api//
 app.get('/', async (req, res) => {
   const posts = await prisma.user.findMany({
   })
   res.json(posts)
 })
+
+//function to filter out the password//
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
+}
+
 
 //post api to post data into database//
 app.post(`/user`, async (req, res) => {
@@ -23,8 +32,21 @@ app.post(`/user`, async (req, res) => {
       email,
       password,
     },
+  }).then(user => {
+    //call the filter function to filter out the password.//
+    return res.json(filter(user, 'id', 'name', 'email'))
+  }).catch(err => {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'email already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      });
+    }
+    throw err
   })
-  res.json(result)
+  
 })
 
 app.listen(port, () => {
