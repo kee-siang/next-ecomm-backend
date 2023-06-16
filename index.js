8593a64ad260d90967cae9ee6d5e87ef665f0f5f
kee-siang <tksiang.ss@example.com>
2023-06-16 15:38:32 +0800

    Revert "encrypt password"
    This reverts commit 0f94d22bc91a65f262169f6364e8ffba96a01338.

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-// user sign in backend // 
+// user sign up backend // 
+
 import express from "express"
 import prisma from "./src/utils/prisma.js"
 import cors from 'cors';
-// import { Prisma } from "@prisma/client";
-import bcrypt from "bcryptjs"
+import { Prisma } from "@prisma/client";
 
 const app = express()
 const port = process.env.PORT || 8080
 app.use(express.json())
 app.use(cors());
 
-//post /sign-in endpoint//
-app.post('/sign-in', async (req, res) => {
-  const data = req.body
+//function for validation//
+function validateUser(input) {
+  const validationErrors = {}
+  if (!('name' in input) || input['name'].length == 0) {
+    validationErrors['name'] = 'cannot be blank'
+  }
 
-  //find respective email address in database to sign in//
-  const user = await prisma.user.findUnique({
-    where: {
-      email: data.email
-    }
+  if (!('email' in input) || input['email'].length == 0) {
+    validationErrors['email'] = 'cannot be blank'
+  }else if ('email' in input && !input['email'].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    validationErrors['email'] = 'is invalid'
+  }
+
+  if (!('password' in input) || input['password'].length == 0) {
+    validationErrors['password'] = 'cannot be blank'
+  }
+
+  if ('password' in input && input['password'].length < 8) {
+    validationErrors['password'] = 'should be at least 8 characters'
+  }
+
+  return validationErrors;
+}
+
+// get data api//
+app.get('/', async (req, res) => {
+  const posts = await prisma.user.findMany({
   })
-  
-  //if the match input is not match with the email that store in the database, send error message//
-  if (!user) return res.status(401).send({
-    error: 'Email address or password not valid'
+  res.json(posts)
+})
+
+//function to filter out the password//
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
+}
+
+
+//post api to post data into database//
+app.post(`/user`, async (req, res) => {
+  const { name, email, password } = req.body
+
+  //validate input from user//
+  const validationErrors = validateUser({ name, email, password })
+
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
+  })
+
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password,
+    },
+  }).then(user => {
+    //call the filter function to filter out the password.//
+    return res.json(filter(user, 'id', 'name', 'email'))
+  }).catch(err => {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'email already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      });
+    }
+    throw err
   })
 
   //check the password is match with database password, if not show error message//
