a653bf1e41f1e07d7b5810a59c9999ac52ec603f
kee-siang <tksiang.ss@example.com>
2023-07-01 15:40:18 +0800

    Revert "Merge pull request #8 from kee-siang/feature/user_can_upload_image"
    This reverts commit 29495b63730bc958a28c11bddc7f9b0991989815, reversing
    changes made to 08753d386416248a292954aaf936ed6594584c8f.

--- app.js
+++ app.js
@@ -3,24 +3,13 @@ import express from "express"
 import cors from 'cors'
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
-import imgRouter from "./src/controllers/img.controllers.js"
-import loadRouter from "./src/controllers/load.controllers.js"
-import checkoutRouter from "./src/controllers/payment.controllers.js"
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 app.use(morgan('combined'))
 app.use('/user', userRouter)
 app.use('/auth', authRouter)
-app.use('/upload', imgRouter)
-app.use('/load', loadRouter)
-app.use('/create-checkout-session', checkoutRouter)
-// start added
-// app.get('/protected', auth, (req, res) => {
-//   res.json({ "hello": "world" })
-// })
-// end added
 
 export default app
 
